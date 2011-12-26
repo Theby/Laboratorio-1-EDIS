8e6f4d2e6a3c6371119312a0e3fc1d06ae5fe583
Felipe Garay <netstatum@gmail.com>
2011-12-26 20:22:38 -0300

    Se acelera el calculo
    Le agrege soporte para leer datos precalculados de una matriz para hacelerar los
    calculos. Voy a poner un array en un txt con varios valores calculados para
    leerlos ese archivo.

--- principal.m
+++ principal.m
@@ -1,4 +1,10 @@
-function retorno = principal(t)
+function retorno = principal(t, valores)
+%t: es el tiempo discreto
+%valores: son los retornos conocidos de la funcion asi
+%valores(1) => valor del tiempo 2
+%valores(2) => valor del tiempo 3
+
+
 Kd=1; %constante a fijar
 Kf=1; %constante a fijar
 Dt=1; %nivel desceado del inventario, trabajamos  con o(t)?
@@ -7,5 +13,10 @@ t1=1; %retardo de los empleados flojos e ineficientes <.<
 if t==1 || t==0
 	retorno=1;
 else
-	retorno=principal(t-1)-Kd*principal(t-1-t1)+Kd*Dt+Kf*o_cero(t-1);
+	%antes que hacer cualquier calculo vemos si ya tenemos calculado el t
+	if(length(valores)>=t-1)
+		retorno=valores(t-1);
+	else
+		retorno=principal(t-1, valores)-Kd*principal(t-1-t1, valores)+Kd*Dt+Kf*o_cero(t-1);
+	end
 end
