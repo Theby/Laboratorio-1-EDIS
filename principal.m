a9f5d62abe9242c3065cb6c5b0c8c3c7fad22c08
Esteban Gaete <zoraskiller@gmail.com>
2011-12-26 00:17:11 -0300

    arreglada la cosntante tl, que es t1 en realidad

--- principal.m
+++ principal.m
@@ -2,12 +2,10 @@ function retorno = principal(t)
 Kd=1; %constante a fijar
 Kf=1; %constante a fijar
 Dt=1; %nivel desceado del inventario, trabajamos  con o(t)?
-
-%que es tl?
-tl=1;
+t1=1; %retardo de los empleados flojos e ineficientes <.<
 
 if t==1 || t==0
 	retorno=1;
 else
-	retorno=principal(t-1)-Kd*principal(t-1-tl)+Kd*Dt+Kf*o_cero(t-1);
+	retorno=principal(t-1)-Kd*principal(t-1-t1)+Kd*Dt+Kf*o_cero(t-1);
 end
